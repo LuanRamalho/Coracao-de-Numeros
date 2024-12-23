1c528762eaf2b409740638a25b97446cf9aa5aec
Luan Ramalho <32373552+LuanRamalho@users.noreply.github.com>
2024-12-23 13:57:58 -0300

    Add files via upload

---
+++ Coração de Númreos.py
@@ -0,0 +1,17 @@
+def exibir_coracao():
+    padrao = [
+        "   1     1   ",
+        "  121   121  ",
+        " 12321 12321 ",
+        "12343211234321",
+        " 12343234321 ",
+        "  123432321  ",
+        "   12321     ",
+        "    121      ",
+        "     1       "
+    ]
+    for linha in padrao:
+        print(linha)
+
+exibir_coracao()
+input()
